e00d468432469eef99bb714fa60664c1a66a22e5
lcmiccioni <miccioni.luca@gmail.com>
2015-05-06 13:59:33 +0200

    Commit

--- main.c
+++ main.c
@@ -19,6 +19,37 @@ struct s_studente {
 typedef struct s_studente studente;
 
 int main(int argc, char** argv) {
+    FILE *outFile;
+    studente alunno;
+    int i;
+    
+    outFile = fopen(FNAME, "w");
+    if(outFile){
+        fprintf(outFile, "nome;cognome;eta';classe\n");
+
+        for(i=0;i<N;i++) {
+            printf("Inserisci il nome: ");
+            scanf("%s", alunno.nome);
+            fprintf(outFile, "%s;", alunno.nome);
+
+            printf("Inserisci il cognome: ");
+            scanf("%s", alunno.cognome);
+            fprintf(outFile, "%s;", alunno.cognome);
+
+            printf("Inserisci l'eta': ");
+            scanf("%d", &alunno.eta);
+            fprintf(outFile, "%d;", alunno.eta);
+
+            printf("Inserisci la classe: ");
+            scanf("%s", alunno.classe);
+            fprintf(outFile, "%s\n", alunno.classe);
+            printf("\n");
+        }
+
+        fclose(outFile);
+    }else {
+        printf("Impossibile creare il file");
+    }
     
     return (EXIT_SUCCESS);
 }
